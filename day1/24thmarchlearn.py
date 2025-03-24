e4b27b9a63899c7a9958a246e40ee8bfabfa6f36
Dipak Paudel <paudelsantosh508@gmail.com>
2025-03-24 23:56:34 +1100

    variables, data type and exponents

--- day1/24thmarchlearn.py
+++ day1/24thmarchlearn.py
@@ -7,3 +7,42 @@
 # install and work with python 3, jupyter notebook, and ipython
 # build python projects that tackel real data
 # create a portfolio project
+
+# first thing in todays lecture was comment 
+
+# in python we write a comment using # 
+
+
+# after that i learnt about print function which is used to output the result
+print("Hello world!")
+
+# once we run this file we can see the output hello world in the console.
+
+# after print function we learnt string which is basically a series of text data either in single quote or double quote
+print("hello world!")
+
+# and 
+print('Hello world')
+# both gives the same result
+
+
+# and to store the data we use variables and in python variables are assigned using = operator
+greeting = 'hello world'
+print(greeting)
+
+# this is the example of the exponent that python program uses
+
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+
+# 8 squared, or 64
+print(8 ** 2)
+
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 ** 3)
+
+# We can even perform fractional exponents
+# 4 to the half power, or 2
+print(4 ** 0.5)
+
+
