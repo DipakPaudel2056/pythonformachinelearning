6a672559ca66bfa20899202f16424fcdb0288cc0
Dipak Paudel <paudelsantosh508@gmail.com>
2025-03-24 23:27:03 +1100

    what are we actually be doing?

---
+++ day1/24thmarchlearn.py
@@ -0,0 +1,9 @@
+# but why we need python for data science?
+# why arent we using other programming languages?
+# how big is its community?
+
+# in the python fundamental one we will be able to write python programs, manage programs using if/else , store data using python list
+# iterate over lists using loops and list comprehension
+# install and work with python 3, jupyter notebook, and ipython
+# build python projects that tackel real data
+# create a portfolio project
