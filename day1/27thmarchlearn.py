6e9256d4098cd1eff4f87b8d294cb5c2c336facc
Dipak Paudel <paudelsantosh508@gmail.com>
2025-03-27 21:58:53 +1100

    parameters and arguments

---
+++ day1/27thmarchlearn.py
@@ -0,0 +1,13 @@
+# parameters and arguments
+# whenever we want a function to get a input value then we use parameters
+# we use those parameters inside the paranthesis
+
+def my_function(singleParam):
+    print(f"This is a function that takes one parameter: {singleParam}")
+
+
+# but how do we use these parameters
+# when we want to call a function we use these parameters by passing arguments
+
+# let's call the previous funtion
+my_function("first parameter")
