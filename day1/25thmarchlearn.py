216b00e8f2025f22f0e4d4419dec677b015fceef
Dipak Paudel <paudelsantosh508@gmail.com>
2025-03-27 21:54:14 +1100

    written my first function

--- day1/25thmarchlearn.py
+++ day1/25thmarchlearn.py
@@ -46,3 +46,15 @@ def create_function():
 
 # this is how you call the function
     create_function()
+
+
+# this is the basic weather_check function i created
+# Your code below: 
+print("Checking the weather for you!")
+def weather_check():
+  print("Looks great outside! Enjoy your trip ")
+print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+
+
+
+weather_check()
