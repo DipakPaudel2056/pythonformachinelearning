0288cdf36408ac5ba3c30339beb92b75c71bb49a
Dipak Paudel <paudelsantosh508@gmail.com>
2025-03-27 22:05:08 +1100

    codecademy first parameter exercise

--- day1/27thmarchlearn.py
+++ day1/27thmarchlearn.py
@@ -11,3 +11,14 @@ def my_function(singleParam):
 
 # let's call the previous funtion
 my_function("first parameter")
+
+
+
+# codecademy tutorial
+def generate_trip_instructions(location):
+  print("Looks like you are planning a trip to visit "+  location)
+  print("/nYou can use the public subway system to get to "+ location)
+
+generate_trip_instructions("Central Park")
+generate_trip_instructions("Grand Central Station")
+
