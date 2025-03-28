e431135003ea37f75b0fecf0c033175a2b5ea7b3
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-03-28 20:51:39 +1100

    medical project

--- day1/28thmarchlearn.py
+++ day1/28thmarchlearn.py
@@ -1,1 +1,3 @@
 # starting of the 4th day of python
+# i will be doing the project of codecademy all on my own
+# i will first do the project in the web editor and then i will paste the final code in here
