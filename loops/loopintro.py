26048697077b11c01e9fc14336c2de07416ea2d6
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-04-08 23:27:30 +1000

    codecademy mini project

--- loops/loopintro.py
+++ loops/loopintro.py
@@ -13,3 +13,18 @@ print(scaled_grades)
 heights = [161, 164, 156, 144, 158, 170, 163, 163, 157] #list of the heights of the people
 can_ride_coaster = [height for height in heights if height > 161 ] # list comprehension to check if the height is greater than 161
 print(can_ride_coaster)
+
+
+# mini project from codecademy
+
+single_digits= [] #creating a single digits empty list
+for i in range(10):
+  single_digits.append(i) #using a normal for loop i populated an empty list
+squares = []
+for element in single_digits:
+  print(element)
+  squares.append(element ** 2) # populated the squares list with the square value
+
+print(squares)
+cubes = [cube**3 for cube in single_digits ]
+print(cubes)
