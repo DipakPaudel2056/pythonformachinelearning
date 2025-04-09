9f0c782bf16d828bcc7bac000b47a3bb0bd3cf42
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-04-10 09:36:41 +1000

    first panda feel

--- dataextraction/intro.py
+++ dataextraction/intro.py
@@ -14,3 +14,15 @@ r_json = r.json() #converted the encrypted r to json to make it easy to work wit
 with open('commute_data.csv',mode='w',newline='') as file: #here we have opened an empty commute_data.csv file in writing mode and newline as file
   writer = csv.writer(file) #writer method on the file
   writer.writerows(r_json) #written the json to the csv file
+
+
+# after we get the csv populated we want to make change in that csv
+# for that we are using a library called pandas 
+import pandas
+# first we want to convert the csv into the data_frame object
+commute_df = pandas.read_csv('commute_data.csv')
+# so we have got this data frame lets see the head
+print(commute_df.head())
+# it prints first few rows of the csv data and we want to make the change in the column name 
+commute_df.columns = ['name','code1', 'code2', 'state', 'county'] #it is actually assigning a new name to the columns name
+print(commute_df.head())
