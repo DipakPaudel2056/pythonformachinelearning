90d1571d1ef92f43e4e720f702149a0199f3160a
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-04-09 23:30:00 +1000

    Frida kahlo retrospective project

--- loops/loopcodecademyproject.py
+++ loops/loopcodecademyproject.py
@@ -25,3 +25,22 @@ for i in range(len(names)):
   
   updated_estimated_costs = [cost*1.1 for cost in estimated_insurance_costs] #list comprehension to create new list which is 1.1 times the original list
   print(updated_estimated_costs)
+
+
+# another micro project 
+paintings = ['The Two Fridas','My Dress Hangs Here','Tree of Hope','Self Portrait With Monkeys']
+dates=[1939,1933,1946,1940]
+paintings = list(zip(paintings,dates)) #created new zip list
+paintings.append(('The Broken Column',1944))
+paintings.append(('The Wounded Deer',1946))
+paintings.append(('Me and My Doll',1937))
+print(paintings)
+length = len(paintings)
+print(length)
+audio_tour_number = []
+for i in range(len(paintings)):
+   audio_tour_number.append(i+1)
+
+print(audio_tour_number)
+master_list = list(zip(audio_tour_number,paintings))
+print(master_list)
