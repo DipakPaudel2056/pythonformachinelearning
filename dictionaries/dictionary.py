983678fd047dce40f40603a66ea497e66dce946f
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-04-14 22:18:28 +1000

    .keys method

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -48,3 +48,12 @@ power_value = available_items.pop("power stew")
 health_points += power_value
 health_points += available_items.pop("mystic bread",0) #it is checking if the mystic bread is in dictionary and pass the default value of 0 if mystic bread is not in the dictionary
 print(available_items,health_points)
+
+# sometime if we want to get the access to all the keys of the dictionaries we can use .keys method
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+print(users)
+print(lessons)
