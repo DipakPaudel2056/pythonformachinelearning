b5b23e217f98f3e6bffca3609f82e5c2b2904b59
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-04-23 13:13:34 +0800

    written in the csv file

--- file/csvReader.py
+++ file/csvReader.py
@@ -15,11 +15,23 @@ with open('file\csvfile.csv') as temp_csv_file:
         
         
 # till now we have only dealt with the csv files separated by comma but we also have other kinds of csv file where the delimiter are different
-import csv
-with open('books.csv') as books_csv:
-  books_reader = csv.DictReader(books_csv,delimiter='@') #here the delimiter is @ 
-  isbn_list = []
-  for row in books_reader:
-    isbn_list.append(row['ISBN'])
+# import csv
+# with open('books.csv') as books_csv:
+#   books_reader = csv.DictReader(books_csv,delimiter='@') #here the delimiter is @ 
+#   isbn_list = []
+#   for row in books_reader:
+#     isbn_list.append(row['ISBN'])
 
-print(isbn_list)
+# print(isbn_list)
+
+
+# as we can read the csv file in the dictionary format we can also write the list into the csv format 
+access_log = [{'time': '08:39:37', 'limit': 844404, 'address': '1.227.124.181'}, {'time': '13:13:35', 'limit': 543871, 'address': '198.51.139.193'}, {'time': '19:40:45', 'limit': 3021, 'address': '172.1.254.208'}, {'time': '18:57:16', 'limit': 67031769, 'address': '172.58.247.219'}, {'time': '21:17:13', 'limit': 9083, 'address': '124.144.20.113'}, {'time': '23:34:17', 'limit': 65913, 'address': '203.236.149.220'}, {'time': '13:58:05', 'limit': 1541474, 'address': '192.52.206.76'}, {'time': '10:52:00', 'limit': 11465607, 'address': '104.47.149.93'}, {'time': '14:56:12', 'limit': 109, 'address': '192.31.185.7'}, {'time': '18:56:35', 'limit': 6207, 'address': '2.228.164.197'}]
+fields = ['time', 'address', 'limit']
+
+import csv
+with open('file\logger.csv','w') as logger_csv: #we opened the logger csv file as logger_csv in writing mode
+   log_writer = csv.DictWriter(logger_csv,fieldnames=fields) #we used dictwriter method to writeinto the csv file and keyword argument fieldnames is set to fields list
+   log_writer.writeheader() #we have used writeheader to create a header of the csv file using the previous step
+   for row in access_log : #loop throught the log and wriiten the row in th row
+    log_writer.writerow(row)
