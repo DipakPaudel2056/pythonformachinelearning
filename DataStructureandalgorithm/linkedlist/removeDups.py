8ebefeb313582afefaeb86990b9f9b712a4a9345
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-03 17:17:04 +0800

    2 sum implementation in linked list

--- DataStructureandalgorithm/linkedlist/removeDups.py
+++ DataStructureandalgorithm/linkedlist/removeDups.py
@@ -44,4 +44,29 @@ class LinkedList:
     
    
               
-        
+# implement the 2 sum problem using linked list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def add_two_numbers(l1, l2):
+    dummy_head = ListNode(0)
+    current = dummy_head
+    carry = 0
+
+    while l1 or l2 or carry: #the loop continues as long as there is any of value in l1 or l2 or carry
+        val1 = l1.val if l1 else 0 #if the l1 doesnot have any value it is 0
+        val2 = l2.val if l2 else 0 #if tthe l2 doesnot have any value it is 0
+
+        total_sum = val1 + val2 + carry
+        carry = total_sum // 10
+        digit = total_sum % 10
+
+        current.next = ListNode(digit)
+        current = current.next
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+
+    return dummy_head.next
