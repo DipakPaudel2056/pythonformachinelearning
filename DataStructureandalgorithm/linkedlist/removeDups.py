d14853de7c716faa5846ca2e59aaeb24d368ac62
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-03 16:36:26 +0800

    k to last element algorithm implemented using 2 pointer

--- DataStructureandalgorithm/linkedlist/removeDups.py
+++ DataStructureandalgorithm/linkedlist/removeDups.py
@@ -7,8 +7,32 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    def removedups(self):
+        if not self.head:
+            return
+        seen = set()
+        current = self.head
+        seen.add(current.data)
         
-    def removeDups(linkedList):
-        hashmap = {}
-        pervious = None
-        while
+        while current.next:
+            if current.next.data in seen:
+                current.next = current.next.next
+            else:
+                seen.add(current.next.data)
+                current = current.next
+    def kth_last(self,k):
+        left = self.head
+        right = self.head
+        if self.head == None:
+            return 0
+        for i in range(k):
+            if (left.next == None):
+                return None
+            left = left.next
+        while (left.next != None):
+            left = left.next
+            right = right.next
+        return right
+   
+              
+        
