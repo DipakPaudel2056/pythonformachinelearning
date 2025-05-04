e61542917b5ff58bc40aad43326d94f5380ac746
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-04 21:11:04 +0800

    lambda function with condition

--- panda/lambdafunction.py
+++ panda/lambdafunction.py
@@ -7,3 +7,22 @@ contains_a = lambda word: "a" in word
 print(contains_a("banana"))
 print(contains_a("apple"))
 print(contains_a("cherry"))
+
+
+#this lambda function long_string 
+long_string = lambda str:len(str) > 12
+
+print(long_string("short"))
+print(long_string("photosynthesis"))
+
+#this lambda function check if the string ends in character a 
+ends_in_a = lambda str:str[-1] == "a"
+
+print(ends_in_a("data"))
+print(ends_in_a("aardvark"))
+
+# we can also add if clause in the lamba function in this way
+#lambda function with if clause looks like this:
+double_or_zero = lambda num: num * 2 if num > 10 else 0
+print(double_or_zero(15))
+print(double_or_zero(5))
