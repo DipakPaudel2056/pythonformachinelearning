d5e14f3dd69d90e7eda004a37ee34a14af7995a6
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-04 21:53:41 +0800

    python numpy and panda brief intro

---
+++ panda/intro.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+list1 = [1,2,3,4]
+# create numpyarrays
+print(np.array(list1))
+
+# to get the 2 dimensional ndarray
+list2 = [[1,2,3],[4,5,6]]
+print(np.array(list2))
+
+
