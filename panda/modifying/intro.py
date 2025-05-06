fa74b07bf3d26508ad1e91d30192fb2032c1eb85
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-06 20:23:55 +0800

    apply method

--- panda/modifying/intro.py
+++ panda/modifying/intro.py
@@ -2,12 +2,12 @@
 import pandas as pd
 
 df = pd.DataFrame([
-  [1, '3 inch screw', 0.5, 0.75],
-  [2, '2 inch nail', 0.10, 0.25],
-  [3, 'hammer', 3.00, 5.50],
-  [4, 'screwdriver', 2.50, 3.00]
+  [1,'Anurag', '3 inch screw', 0.5, 0.75],
+  [2,'Saksham', '2 inch nail', 0.10, 0.25],
+  [3,'Saurav', 'hammer', 3.00, 5.50],
+  [4,'Aaryan', 'screwdriver', 2.50, 3.00]
 ],
-  columns=['Product ID', 'Description', 'Cost to Manufacture', 'Price']
+  columns=['Product ID','Name', 'Description', 'Cost to Manufacture', 'Price']
 )
 # Add columns here
 # here we have manually added a new column with manually typing the data
@@ -16,4 +16,6 @@ df['Sold in Bulk?'] = ['Yes','Yes','No','No']
 df['is taxed?'] = 'Yes'
 # we can also perform the operation in between the values of the column and create new column in this way
 df['margin'] = df['Price'] - df['Cost to Manufacture']
+# we can also make the use of apply function to make the change in the column
+df['Lowercase Name'] = df.Name.apply(str.lower) 
 print(df)
