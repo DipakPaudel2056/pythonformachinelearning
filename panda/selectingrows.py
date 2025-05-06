bab2d70ebd425bc9ee7b903cc2c48e3ffa4bac3c
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-06 12:51:18 +0800

    multiple logic row select

--- panda/selectingrows.py
+++ panda/selectingrows.py
@@ -17,4 +17,17 @@ df = pd.DataFrame([
 march = df.iloc[2]
 # we can also select the number of rows using
 april_may_june = df.iloc[3:6] #this will select the row from 3 to 6 not including 6
-print(march)
+
+# we can also using conditional logic to find the specific data using panda
+# to find all the records of data of january 
+january = df[df.month == "January"]
+# find the rows whree clinic_east > 60
+clinic_east_greater_than_60 = df[df.clinic_east > 60]
+print(clinic_east_greater_than_60)
+
+
+# we can also add as many logic as we can inside the [] as long as the logics are separated by paranthesis
+# in the example below we are finding the data that are of march or april
+
+march_april = df[(df.month == "March") | (df.month == "April")]
+print(march_april)
