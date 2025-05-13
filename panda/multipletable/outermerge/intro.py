3c4c03a915c3e1c17608283ba66175382b0ce07a
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-13 11:25:11 +0800

    left merge --multipletable

--- panda/multipletable/outermerge/intro.py
+++ panda/multipletable/outermerge/intro.py
@@ -13,3 +13,6 @@ store_b = pd.read_csv('panda/multipletable/store_b.csv')
 store_a_b = pd.merge(store_a,store_b,how='outer')
 print(store_a_b) #we have seen that the data which donot have the value on the given table are displayed as NaN 
 
+# lets do left and right merge, left merge is the condition where we get all the items from the left table and the value from the right table be nan and in the right merge we get all the value from the right table and only the matching value from the right table aif not then the nan
+store_a_b_left = pd.merge(store_a,store_b,how='left')
+store_b_a_left = pd.merge(store_b,store_a,how='left')
