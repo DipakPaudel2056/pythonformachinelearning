487e6d70fd9c9328fc0c9e7e14a175ce080d0682
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-19 12:29:09 +0800

    zooming in and out the data

--- matplotlib/intro.py
+++ matplotlib/intro.py
@@ -14,7 +14,6 @@ y = [0,1,4,9,16]
 z = [0,1,8,27,64]
 plt.plot(x,y)
 plt.plot(x,z)
-plt.show()
 
 # lets see the tutorial from codecademy
 time = [0, 1, 2, 3, 4]
@@ -23,4 +22,9 @@ costs = [150, 500, 550, 550, 560]
 
 plt.plot(time,revenue,color='purple',linestyle='--')
 plt.plot(time,costs,color='#82edc9',marker='s')
+
+# here we donot have the control on how much of zoom is required on the chart
+# to deal with this issue we want to use .axis method and pass min-x,max-x,min-y,max-y 
+plt.plot(time,revenue)
+plt.axis([0,5,100,900])
 plt.show()
