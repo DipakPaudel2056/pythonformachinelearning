b9d01e20f080415b72db24c6602eb4f75fda22b2
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-19 12:50:30 +0800

    added subplots

--- matplotlib/intro.py
+++ matplotlib/intro.py
@@ -25,8 +25,6 @@ plt.plot(time,costs,color='#82edc9',marker='s')
 
 # here we donot have the control on how much of zoom is required on the chart
 # to deal with this issue we want to use .axis method and pass min-x,max-x,min-y,max-y 
-plt.axis([0,5,100,900])
-
 
 # we can also make the change in the name of the x-axis y-axis and the title to do so we use
 #.xlabel
@@ -36,4 +34,16 @@ plt.plot(time,revenue)
 plt.xlabel('time')
 plt.ylabel('revenue')
 plt.title('revenue vs time')
+
+
+# subplot are the plots that are plotted in the same figure and it takes the parameter that defins the x value, y-value and the order of the plot
+# for example if we want to plot 2 plot side by side which is in the [1,2] matrix and the other parameter is 1, or 2 based on the order
+
+# lets display 2plots in the same figure i am trying to plot time vs rev in 1 and rev vs costs in 2
+plt.subplot(1,2,1)
+plt.plot(time,revenue)
+
+plt.subplot(1,2,2)
+plt.plot(costs,revenue, "o")
+
 plt.show()
